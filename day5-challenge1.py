ed9ee1c3a473835bfea29ff8c778d1aa6a2810c1
deecewan <david.buchanswanson@gmail.com>
2015-12-10 12:31:58 +0000

    20%

---
+++ day5-challenge1.py
@@ -0,0 +1,21 @@
+# NB. this did not pass all tests.  it came up 2 strings short of the goal.
+# I do not know why.
+
+input = open('day5.input')
+
+import re
+
+double_letters = re.compile(r"(.)\1")
+vowels = re.compile(r"([aeiou])")
+bad = re.compile(r"ab|bc|pq|xy")
+
+count = 0
+line_no = 1
+
+for line in input:
+    if len(vowels.findall(line)) > 2 and double_letters.findall(line) and not bad.findall(line):
+        count += 1
+        print 'adding one for line ', line_no
+    line_no += 1
+        
+print count
